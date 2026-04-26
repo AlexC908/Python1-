4d9012bb5ea48b8b86676ea736c0d453885aedc2
AlexC07 <cespedeschoquealex0707@gmail.com>
2026-04-26 00:43:30 -0400

    NumerosPar7Impar

---
+++ tareaif1.py
@@ -0,0 +1,11 @@
+#Reto 3: Par o Impar (Avanzado)
+#Objetivo: Introducir el operador módulo %.
+#Instrucciones: Pide un número. Si numero % 2 == 0, es par. Si no, es impar.
+
+numero = int(input("Cual es su numero?"))
+
+if numero %2 == 0:
+  print("Es numero par")
+else:
+  print("Es numero impar")
+  
